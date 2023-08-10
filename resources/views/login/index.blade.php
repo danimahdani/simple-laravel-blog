d2d3c4a14076e76389c99b38147cdbdedcf53054
mahdanidn <mahdanidn@gmail.com>
2023-08-10 10:59:55 +0700

    success login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,25 +11,25 @@
       </div>
     @endif
 
-    {{-- @if(session()->has('loginError'))
+    @if(session()->has('loginError'))
       <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{ session('loginError') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
-    @endif --}}
+    @endif
 
     <main class="form-signin">
       <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
       <form action="/login" method="post">
-        {{-- @csrf --}}
+        @csrf
         <div class="form-floating">
           <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
           <label for="email">Email address</label>
-          {{-- @error('email')
+          @error('email')
             <div class="invalid-feedback">
               {{ $message }}
             </div>
-          @enderror --}}
+          @enderror
         </div>
         <div class="form-floating">
           <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,7 +44,11 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
